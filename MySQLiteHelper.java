87f202199355f038a6e52a0a0004163de740bae7
Davis-eng <davisngigi@gmail.com>
2018-01-21 10:26:26 +0300

    Create MySQLiteHelper.java

---
+++ MySQLiteHelper.java
@@ -0,0 +1,36 @@
+package ke.co.microhub.e_data;
+
+/**
+ * Created by davis eng on 2016-07-06.
+ */
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MySQLiteHelper extends SQLiteOpenHelper
+{
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "UserLists";
+
+    public MySQLiteHelper(Context context)
+    {
+        //specify our database name and version
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db)
+    {
+		/* Do nothing here. Empty database will be created when getWritableDatabase is called
+		tables represent a users item lists which are created by the user at runtime*/
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+    {
+        //No Upgrades planned, do nothing
+    }
+
+}
+
